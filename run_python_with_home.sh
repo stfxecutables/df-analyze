a8b48c911b7140ae2c848185c734dda8467e85e2
Derek Berger <dmberger.dev@gmail.com>
2024-01-08 14:41:41 -0500

    WIP

--- run_python_with_home.sh
+++ run_python_with_home.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-export MLPCONFIGDIR="$(readlink -f .)"/.mplconfig
+export APPTAINERENV_MLPCONFIGDIR="$(readlink -f .)"/.mplconfig
+export APPTAINERENV_OPENBLAS_NUM_THREADS="1"
 apptainer run --home $(readlink -f .) --app python debian_app.sif $@
