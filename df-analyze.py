9d77e4796648080c31b85584bd107051e9fec825
Derek Berger <dmberger.dev@gmail.com>
2024-06-13 16:30:45 -0400

    SHARE

--- df-analyze.py
+++ df-analyze.py
@@ -1,3 +1,13 @@
+from __future__ import annotations
+
+# fmt: off
+import sys  # isort: skip
+from pathlib import Path  # isort: skip
+ROOT = Path(__file__).resolve().parent  # isort: skip
+sys.path.append(str(ROOT))  # isort: skip
+# fmt: on
+
+
 import sys
 import traceback
 from contextlib import redirect_stderr, redirect_stdout
