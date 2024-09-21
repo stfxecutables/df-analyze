d377f9a2003b82400cb2ada486960394656d0795
Derek Berger <dmberger.dev@gmail.com>
2024-09-21 14:57:05 -0400

    WIP

--- src/df_analyze/__init__.py
+++ src/df_analyze/__init__.py
@@ -1,3 +1,11 @@
+import sys
+from pathlib import Path
+
+ROOT = Path(__file__).resolve().parent.parent.parent
+SRC = ROOT / "src"
+sys.path.append(str(SRC))
+
+
 from df_analyze._main import main
 # def main() -> int:
 #     print("Hello from rye-learn!")
