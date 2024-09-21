fe8655735eabf73a8cf97e7bda9cd0815f9014f6
Derek Berger <dmberger.dev@gmail.com>
2024-09-21 15:02:21 -0400

    WIP

--- run_python_with_home.sh
+++ run_python_with_home.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+
+PROJECT=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+ROOT="$(dirname "$PROJECT")"
+cd "$ROOT" || exit 1
+echo "Running script from: $ROOT"
+
+# grotesque hack to deal with shitty python imports never working on CC
+# file system in apptainer
+DF_ANALYZE_SRC="$ROOT/src"
+export PATH="$PATH:$DF_ANALYZE_SRC"
+export APPTAINERENV_PATH="$PATH:$APPTAINERENV_PATH"
+
+# silence matlplotlib warning on readonly filesystems
 export APPTAINERENV_MPLCONFIGDIR="$(readlink -f .)"/.mplconfig
 export APPTAINERENV_OPENBLAS_NUM_THREADS="1"
 apptainer run --home $(readlink -f .) --app python df_analyze.sif $@
