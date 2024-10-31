197e781b82923826bb22fa5a2cbd1c56f467cc76
Derek Berger <dmberger.dev@gmail.com>
2024-10-30 21:02:31 -0400

    WIP - update container build

--- containers/build_container_cc.sh
+++ containers/build_container_cc.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# shellcheck disable=SC2016
-# shellcheck disable=SC2005
+# shellcheck disable=SC2016,SC2005
 THIS_SCRIPT_PARENT="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
 cd "$THIS_SCRIPT_PARENT" || exit 1
 
 # shellcheck disable=SC2094
 rm -f build.txt && bash build_container_cc_alt.sh 2> build.txt | tee -a build.txt
+
