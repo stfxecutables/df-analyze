4841d50bfe45ce57a8819d548e417169786cbaf4
Derek Berger <dberger@stfx.ca>
2024-10-30 20:34:42 -0400

    WIP - update container build

--- containers/build_container_cc.sh
+++ containers/build_container_cc.sh
@@ -4,4 +4,4 @@
 THIS_SCRIPT_PARENT="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
 cd "$THIS_SCRIPT_PARENT" || exit 1
 
-rm -f build.txt && bash build_container_cc_alt.sh 2>&1 | tee build.txt
+rm -f build.txt && bash build_container_cc_alt.sh 2> build.txt | tee -a build.txt
